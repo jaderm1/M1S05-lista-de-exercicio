e58b1a883fa96a74036f02a32263d0fe931d37a8
Jader Luiz Maciel do Nascimento <jadermaciel1@gmail.com>
2023-06-13 20:37:07 -0300

    exercicio 2 concluido

--- exercicio2.js
+++ exercicio2.js
@@ -5,3 +5,15 @@
  * a tradução para a mesma cor em português. Utiliza uma estrutura condicional (switch)
  * para fazer o mapeamento das cores.
  */
+
+
+let color = ['red', 'green', 'blue', 'purple',]
+
+switch (color[0]) {
+    case'red':console.log("vermelho");
+    case 'green':console.log("verde");
+    case 'blue':console.log("azul");
+    case 'purple':console.log("roxo");
+    
+
+}
