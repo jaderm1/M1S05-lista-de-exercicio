159d2c892bff7c7ab16b623d4284112994dc497e
Jaderm1 <jadermaciel1@gmail.com>
2023-06-18 18:50:20 -0300

    Exercicio-6 concluido

--- exercicio6.js
+++ exercicio6.js
@@ -5,3 +5,24 @@
  * e o preço de cada produto. Após imprimir essas informações, imprime o total de produtos
  * e o valor total de todos os produtos.
  */
+
+
+let products = [
+
+    { 'name': 'Coca-Cola', 'preço': 5.00 }, 
+    { 'name': 'Pepsi', 'preço': 4.50 },
+    { 'name': 'Fanta', 'preço': 4.00 },
+    { 'name': 'Guaraná', 'preço': 3.50 }]
+
+    let priceAllProducts = 0;
+
+for (let i = 0; i < products.length; i++) {
+    console.log(products[i].name + ' R$ ' + products[i].preço);
+priceAllProducts = priceAllProducts + products[i].preço;
+}
+console.log(`O total de produtos disponivel é ${products.length}`);
+console.log(`O valor total de todos os produtos é ${priceAllProducts}`);
+
+
+
+
