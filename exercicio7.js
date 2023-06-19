d5d867aea7d99e26d546f28232799d664ae0b2a0
Jaderm1 <jadermaciel1@gmail.com>
2023-06-18 22:50:36 -0300

    Exercicio-7 concluido

--- exercicio7.js
+++ exercicio7.js
@@ -2,7 +2,7 @@
  * [M1S05] Ex 7 - Manipulação de arrays
  *
  * Escreve um programa que contém três funções para manipular arrays, utilizando os métodos
- * push, pop e slice:
+ * push, pop e splice:
  *   A. `adicionarElemento`: recebe um array e um elemento, e adiciona o elemento ao final do array.
  *      Em seguida, retorna o array atualizado.
  *   B. `removerUltimoElemento`: recebe um array, remove o último elemento do array e retorna o elemento removido.
@@ -10,3 +10,35 @@
  *      remover os elementos do array compreendidos entre os índices inicial e final. Em seguida, retorna o
  *      array atualizado.
  */
+
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function add(array,elemento){
+
+    array.push(elemento);
+    return array;
+
+}
+
+function removeLast(array){
+
+    
+    return array.pop();
+
+}
+
+function removeFromInterval (array, init , end){
+
+    array.splice(init,end);
+    return array;
+
+}
+
+
+
+console.log(add(array,10));
+
+console.log(removeLast(array));
+
+console.log(removeFromInterval(array,2,4));
