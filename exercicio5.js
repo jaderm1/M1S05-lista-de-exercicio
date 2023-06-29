0893fb21f9ee60856cab64ace0862ae9dd18da5c
Jader Luiz Maciel do Nascimento <jadermaciel1@gmail.com>
2023-06-29 19:02:58 -0300

    exercicio-05 concluido

--- exercicio5.js
+++ exercicio5.js
@@ -3,4 +3,15 @@
  *
  * Escreve um programa que percorre um array de palavras e exibe todas as palavras que possuem
  * mais de 5 caracteres. Utiliza a estrutura de repetição for of para percorrer o array.
- */
+    */
+
+
+let words = ['casa', 'carro','arvores','bicicleta','moto','caminhão','ônibus'];
+
+for (let word of words){
+
+    if (word.length >5){
+console.log(word);
+
+    }
+}
